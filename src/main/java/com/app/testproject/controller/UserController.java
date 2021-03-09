6401ee5bd8abe4e89398bc68e97f595a6cd1e72d
Sofia Bianco <b.sofia2124@gmail.com>
2021-03-09 19:14:21 -0300

    update

--- src/main/java/com/app/testproject/controller/UserController.java
+++ src/main/java/com/app/testproject/controller/UserController.java
@@ -9,7 +9,7 @@ import com.app.testproject.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +18,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+//@Controller
+@RestController
 @RequestMapping(path = "/testApi")
 public class UserController {
 
@@ -42,20 +49,27 @@ public class UserController {
         return user;
     }
 
+    @Operation(summary="Find all the users")
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Success|OK"),
+        @ApiResponse(responseCode = "404", description = "Not Found|Fail")
+    })
     @GetMapping(path = "/all")
     public @ResponseBody Iterable<User> getAll() {
         log.info("Find all users");
         return userRepo.findAll();
     }
 
+    @Operation(summary="Find one user by its ID")
     @GetMapping(path = "/find/{id}")
-    public @ResponseBody Optional<User> getUserById(@PathVariable String id) {
+    public @ResponseBody Optional<User> getUserById(@Parameter(description="User ID") @PathVariable String id) {
         log.info("Find the user with the ID: " + id);
         return userRepo.findById(Integer.valueOf(id));
     }
 
+    @Operation(summary="Find one user by its role")
     @GetMapping(path = "/{role}")
-    public @ResponseBody Iterable<User> getUsers(@RequestParam String role) {
+    public @ResponseBody Iterable<User> getUsers(@Parameter(description="User Role") @RequestParam String role) {
         log.info("Find a user with the Role: " + role);
         return userRepo.findByRole(role);
     }
